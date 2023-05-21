043e29a58ed856f0e9970e06b05b97b3f30e5f2a
AlexM-FullStack <amilg@gmail.com>
2023-05-21 10:02:31 +1000

    new products and module

--- index.js
+++ index.js
@@ -1,36 +1,7 @@
+import shopProductsData from "./products.js"
+
 const shopEl = document.getElementById('shop')
 const cartAmount = document.querySelector('.cartAmount')
-
-let shopProductsData = [
-    {
-        id: 1,
-        name: 'American T',
-        price: 25,
-        description: 'USA flag t shirt',
-        img: 'images/american.png'
-    },
-    {
-        id: 2,
-        name: 'Mountain Bear',
-        price: 25,
-        description: 'Mountain landscape with bear tshirt',
-        img: 'images/bear.png'
-
-    }, 
-    {
-        id: 3,
-        name: 'Guitar T',
-        price: 31,
-        description: 'Guitar logo tshirt',
-        img: 'images/guitar.png'
-    }, 
-    {
-        id: 4,
-        name: 'Sullen T',
-        price: 27,
-        description: 'Classic fit navy tshirt',
-        img: 'images/flying.png'
-    }]
 
 function generateShopContent(){
     return shopEl.innerHTML = shopProductsData.map(product => {
---
+++ products.js
@@ -0,0 +1,61 @@
+let shopProductsData = [
+    {
+        id: 1,
+        name: 'American T',
+        price: 25,
+        description: 'USA flag t shirt',
+        img: 'images/american.png'
+    },
+    {
+        id: 2,
+        name: 'Mountain Bear',
+        price: 25,
+        description: 'Mountain landscape with bear tshirt',
+        img: 'images/bear.png'
+
+    }, 
+    {
+        id: 3,
+        name: 'Guitar T',
+        price: 31,
+        description: 'Guitar logo tshirt',
+        img: 'images/guitar.png'
+    }, 
+    {
+        id: 4,
+        name: 'Sullen T',
+        price: 27,
+        description: 'Classic fit navy tshirt',
+        img: 'images/flying.png'
+    },
+    {
+        id: 5,
+        name: 'Milky Way T',
+        price: 27,
+        description: 'Classic fit navy tshirt',
+        img: 'images/milky way.png'
+    },
+    {
+        id: 6,
+        name: 'Soundbeats Music T',
+        price: 25,
+        description: 'Classic fit black tshirt',
+        img: 'images/music.png'
+    },
+    {
+        id: 7,
+        name: 'Raven T',
+        price: 23,
+        description: 'Classic fit white raven logo T',
+        img: 'images/raven.png'
+    },
+    {
+        id: 8,
+        name: 'Element of Surprise T',
+        price: 27,
+        description: 'Classic fit navy geek logo T',
+        img: 'images/surprise.png'
+    }
+]
+
+export default shopProductsData
